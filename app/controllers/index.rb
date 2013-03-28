888d2335dfad26c5479bb64a9cf1feea1c2bf866
Apprentice <apprentice@devbootcamp.com>
2013-03-28 11:42:35 -0700

    Added extra views, routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,9 +7,20 @@ post '/' do
 
   p = Photo.new
   p.photo_string = params['photo']
-  p.user_id = 5  
+  p.user_id = 5
   p.save!
 
-  redirect "/user/#{current_user.id}"
+  redirect "/i/#{p.short_url}"
 end
-     
+
+get '/i/:short_url' do
+  @photo = Photo.find_by_short_url(params[:short_url])
+  erb :single_view
+end
+
+get '/user/:user_id' do
+  @user = User.find(params[:user_id])
+  @photos = Photo.where('user_id = ?', @user.id)
+
+  erb :user_page
+end
